02bb0c2cc990490a9d1113456afe1d364f6a2327
Svetlana Bykovskaya <bykovskaya.svetlana@gmail.com>
2015-10-11 13:02:18 +0300

    Update Tree_OJ.R

--- Tree Model/Tree_OJ.R
+++ Tree Model/Tree_OJ.R
@@ -1,25 +1,29 @@
-### Required Packages
-#install.packages("tree")
-#install.packages("ISLR")
+# clear variables and close windows
+rm(list = ls(all = TRUE))
+graphics.off()
 
-library("tree")
-library("ISLR")
-library(randomForest)
+# install and load packages
+libraries = c("tree", "ISLR", "randomForest")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+    install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
-## Load the OJ Dataset
+# load data
 data(OJ)
-OJ <- data.frame(OJ)
+OJ = data.frame(OJ)
 
-## Build a Tree Model with all Regressors to fit Purchase (can either by MM or CH)
-tree.oj <- tree(Purchase~.,OJ)
+# Build a Tree Model with all Regressors to fit Purchase (can either by MM or CH)
+tree.oj = tree(Purchase ~ ., OJ)
 summary(tree.oj)
-## Plot the Tree Model
+
+# Plot the Tree Model
 plot(tree.oj)
 text(tree.oj)
 
-## Compare with RandomForests 
-random.oj <- randomForest(Purchase ~ .,OJ)
+# Compare with RandomForests
+random.oj = randomForest(Purchase ~ ., OJ)
 dev.new()
 plot(random.oj)
 print(random.oj)
-summary(random.oj)
+summary(random.oj) 
