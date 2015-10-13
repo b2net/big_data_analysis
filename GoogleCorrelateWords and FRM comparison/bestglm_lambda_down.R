5ae02cc2c007aec85860abb24510953980c6afa7
lborke <lukas.borke@web.de>
2015-10-14 00:20:45 +0200

    edit code + change plot 3

--- GoogleCorrelateWords and FRM comparison/bestglm_lambda_down.R
+++ GoogleCorrelateWords and FRM comparison/bestglm_lambda_down.R
@@ -2,10 +2,6 @@
 rm(list = ls(all = TRUE))
 graphics.off()
 
-# load data
-Lambda_mean = read.csv("Lambda_Zeitreihe_Mai15_weekly.csv", header = TRUE, sep = ";", dec = ",")
-ctest = read.csv("Google_Correlate_with_downturn.csv", header = TRUE, sep = ";", dec = ",")
-
 # install and load packages
 libraries = c("bestglm")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
@@ -13,14 +9,20 @@ lapply(libraries, function(x) if (!(x %in% installed.packages())) {
 })
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
+# load data
+Lambda_mean = read.csv("Lambda_Zeitreihe_Mai15_weekly.csv", header = TRUE, sep = ";", dec = ",")
+ctest = read.csv("Google_Correlate_with_downturn.csv", header = TRUE, sep = ";", dec = ",")
+
+
 # Plot 1: Financial Risk Meter
 plot(Lambda_mean[, 2], type = "l", pch = 1, col = "red", xaxt = "n", xlab = "Date", 
     ylab = "Lambda", main = "Financial Risk Meter", ylim = c(0, 0.15), lwd = 2)
 axis(1, at = c(1:nrow(Lambda_mean)), labels = Lambda_mean[, 1])
 
+# 100 most correlated words with the term "downturn" from Google Trends
+colnames(ctest)
+
 # Erase / Deselect suspicious predictor variables
-ctest
-ctest_clean = data.frame(ctest)
 ctest_clean = subset(ctest, select = -c(acai, acai.berry, banorte.mexico, blow.ya.back.out, 
     X18.days.lyrics, motor.arcade, unknown, motorbike.games, te.regalo.amores.lyrics, 
     break.me.down.if.it.makes.you.feel.right, matlab.2008b, notepad.2008, lions.tigers.and.bears.lyrics, 
@@ -36,10 +38,11 @@ ctest_clean = subset(ctest, select = -c(acai, acai.berry, banorte.mexico, blow.y
     socialvibe, alternext, gdp.2008, us.gdp.2008, mil.demonios, lost.by.gorilla.zoe, 
     synthasite.com, saving.abel.18.days), drop = TRUE)
 
-# bestGLM Regression
+# bestGLM Regression: Google Trends terms on the lambda time series of the Financial Risk Meter
 regsub = regsubsets(x = ctest_clean[184:584, c(2:10)], y = Lambda_mean[1:401, 2], 
     nvmax = 4)
 summary(regsub)
+# Adjusted R-squared
 summary(regsub)$adjr2
 
 # Alternative quality criteria - BIC criteria. The smaller the value the better is the variable/model.
@@ -54,8 +57,9 @@ summary(regsub, all.best = TRUE, matrix = TRUE, matrix.logical = FALSE, df = NUL
 lm_down_eco = lm(Lambda_mean[1:401, 2] ~ ctest_clean[179:579, "down.economy"])
 summary(lm_down_eco)
 
-# Plot 3: Regression
+# Plot 3: Regression: down.economy -- FRM shifted 5 weeks
 dev.new()
-plot(Lambda_mean[1:401, 2] ~ ctest_clean[181:581, "down.economy"], main = "Regression", 
-    ylab = "Lambda Zeitreihe (FRM)", xlab = "Suchbegriff down economy")
+plot(Lambda_mean[1:401, 2] ~ ctest_clean[181:581, "down.economy"],
+    main = "Scatter plot between the search term „down economy“ and the time series of the Financial Risk Meter", 
+    ylab = "Lambda time series (FRM)", xlab = "search term: 'down economy'")
 abline(lm_down_eco, col = "red") 
